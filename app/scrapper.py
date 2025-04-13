112758a53ec5c3d33cbb9baff5588b8bf1b50205
yugal19 <yugalchandak19@gmail.com>
2025-04-14 00:46:06 +0530

    modified:   app/scrapper.py

--- app/scrapper.py
+++ app/scrapper.py
@@ -17,8 +17,8 @@ def setup_driver():
     options = Options()
     options.add_argument("--no-sandbox")
     options.add_argument("--disable-dev-shm-usage")
-    options.add_argument("--headless")
-    options.add_argument("--window-size=1920,1080")
+    # options.add_argument("--headless")
+    # options.add_argument("--window-size=1920,1080")
     return webdriver.Chrome(options=options)
 
 
